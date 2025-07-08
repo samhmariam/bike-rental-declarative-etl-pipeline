df128effbd76a9757130b8154b42be7e8724f072
samhmariam <samhmariam>
2025-07-08 19:54:27 +0000

    03 - transformation

---
+++ 03-transformations/02-bronze.sql
@@ -0,0 +1,26 @@
+-- Let's start out by ingesting our raw files from our UC Volume
+-- ==========================================================================
+-- == Incrementally load RAW RIDES from CSV                                ==
+-- ==========================================================================
+CREATE OR REFRESH STREAMING TABLE RIDES_RAW 
+COMMENT "Raw rides data streamed in from CSV files."
+AS SELECT * FROM 
+  STREAM READ_FILES("/Volumes/${catalog}/${schema}/raw_data/rides/*.csv", FORMAT => "csv");
+
+
+-- ==========================================================================
+-- == Incrementally load RAW MAINTENANCE LOG from CSV                      ==
+-- ==========================================================================
+CREATE OR REFRESH STREAMING TABLE MAINTENANCE_LOGS_RAW
+COMMENT "Raw maintenance logs streamed in from CSV files."
+AS SELECT * FROM 
+  STREAM READ_FILES("/Volumes/${catalog}/${schema}/raw_data/maintenance_logs/*.csv", FORMAT => "csv", MULTILINE => TRUE);
+
+
+-- ==========================================================================
+-- == Incrementally load RAW WEATHER from CSV                              ==
+-- ==========================================================================
+CREATE OR REFRESH STREAMING TABLE WEATHER_RAW
+COMMENT "Raw weather data streamed in from JSON files."
+AS SELECT * FROM 
+  STREAM READ_FILES("/Volumes/${catalog}/${schema}/raw_data/weather/*.json", FORMAT => "json");
